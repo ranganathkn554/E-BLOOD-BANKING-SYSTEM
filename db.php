e87e7719406e47ebfc37b8aedf2c4e00d9c8f2cf
ranganathkn554 <151718339+ranganathkn554@users.noreply.github.com>
2024-12-08 13:21:45 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,17 @@
 <?php
-    // Database connection
+   
     $host = "localhost";
-    $username = "root"; // Replace with your database username
-    $password = ""; // Replace with your database password
+    $username = "root"; 
+    $password = ""; 
     $dbname = "ebloodbank";
 
-    // Create connection
+    
     $conn = new mysqli($host, $username, $password, $dbname);
     if ($conn) {
         echo "Database connection successful!";
     }
     
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
